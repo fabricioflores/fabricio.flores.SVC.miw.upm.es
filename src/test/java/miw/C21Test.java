1fb2df4ced5edc2d4882b11382c75d90b7bf70d1
Fabricio Flores <fabrifloresg@gmail.com>
2016-09-22 20:17:25 +0200

    Test para clase C21 creado

---
+++ src/test/java/miw/C21Test.java
@@ -0,0 +1,26 @@
+package miw;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C21Test {
+	
+	private C21 c21;
+	
+	@Before
+    public void before() {
+        c21 = new C21();
+    }
+
+	@Test
+	public void testM1() {
+		assertTrue(c21.m1().equals("m1"));
+	}
+	
+	public void testM2() {
+		assertTrue(c21.m2().equals("m2"));
+	}
+
+}
